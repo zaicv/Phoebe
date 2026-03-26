8270d0b7185b46d829126fd2f4fbba862a483a30
zaicv <isaiah.jailin18@gmail.com>
2026-03-26 10:58:06 -0500

    Initial Commit

---
+++ Phoebe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Phoebe
+//
+//  Created by zai on 3/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Phoebe/PhoebeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoebeApp.swift
+//  Phoebe
+//
+//  Created by zai on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct PhoebeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
